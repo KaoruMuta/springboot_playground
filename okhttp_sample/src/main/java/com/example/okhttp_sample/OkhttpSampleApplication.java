c80aea0ecbbece6e6d80791f7df385fd6b3a3c45
Kaoru Muta <muttaroni8827@gmail.com>
2023-04-21 00:16:11 +0900

    feat: initialize okhttp sample project with https://github.blog/2023-03-23-we-updated-our-rsa-ssh-host-key/

---
+++ okhttp_sample/src/main/java/com/example/okhttp_sample/OkhttpSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.okhttp_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OkhttpSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OkhttpSampleApplication.class, args);
+	}
+
+}
---
+++ okhttp_sample/src/test/java/com/example/okhttp_sample/OkhttpSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.okhttp_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkhttpSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
