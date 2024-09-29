b450b614d5d41614a25420247f331071364b5270
Kaoru Muta <muttaroni8827@gmail.com>
2024-09-29 16:40:51 +0900

    feat: initialize onion_with_ddd_sample

---
+++ onion_with_ddd_sample/src/main/java/com/example/onion_with_ddd_sample/OnionWithDddSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.onion_with_ddd_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnionWithDddSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnionWithDddSampleApplication.class, args);
+	}
+
+}
---
+++ onion_with_ddd_sample/src/test/java/com/example/onion_with_ddd_sample/OnionWithDddSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onion_with_ddd_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnionWithDddSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
